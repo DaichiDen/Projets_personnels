774f61628609746dcfda2cf028ff895f61edcea2
DaichiDen <165920391+DaichiDen@users.noreply.github.com>
2025-01-04 03:05:59 +0100

    Add files via upload

---
+++ Lanceur.java
@@ -0,0 +1,15 @@
+//Ce programme fait appel à GestionneurDeDessin,c'est le programme principal.
+
+public class Lanceur{
+
+  public static void main(String[] args){
+    GestionneurDeDessin.choisir();
+
+  }
+}
+    
+
+
+
+
+
